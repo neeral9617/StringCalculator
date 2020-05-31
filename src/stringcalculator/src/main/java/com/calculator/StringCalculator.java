8fed85270f697509ef6d5f19f138bbc4cca13557
neeral9617@gmail.com <neeral9617@gmail.com>
2020-05-31 22:13:01 +0530

    Empty string test case

--- src/stringcalculator/src/main/java/com/calculator/StringCalculator.java
+++ src/stringcalculator/src/main/java/com/calculator/StringCalculator.java
@@ -1,5 +1,10 @@
 package stringcalculator.src.main.java.com.calculator;
 
 public class StringCalculator {
-
+	public static int add(final String input) throws Exception {
+		if (input.isEmpty()) {
+			return 0;
+		}
+		return -1;
+	}
 }
--- src/stringcalculator/src/test/java/com/calculator/StringCalculatorTest.java
+++ src/stringcalculator/src/test/java/com/calculator/StringCalculatorTest.java
@@ -1,14 +1,21 @@
 package stringcalculator.src.test.java.com.calculator;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+
+import stringcalculator.src.main.java.com.calculator.StringCalculator;
 
 class StringCalculatorTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void isEmpty() {
+		try {
+			assertEquals(0, StringCalculator.add(""));
+		} catch (final Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
